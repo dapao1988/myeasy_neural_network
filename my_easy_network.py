5ddc536e99ed26e13dc7975afd125df7ab720fda
Wenbing.Wang <wenbing.wang@rokid.com>
2020-06-09 00:38:44 +0800

    add train & test set with epoch, get accuracy 97.28%

--- my_easy_network.py
+++ my_easy_network.py
@@ -5,6 +5,12 @@
 import numpy
 # scipy.special for the sigmoid function expit()
 import scipy.special
+# library for plotting arrays
+import matplotlib.pyplot
+#%matplotlib inline
+#from IPython import get_ipython
+# ensure the plots are inside this notebook, not an external window
+#get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 class MyEasyNetwork:
@@ -85,14 +91,74 @@ class MyEasyNetwork:
 
 if __name__ == '__main__':
     # number of input, hidden and output nodes
-    INPUT_NODES_NUM = 3
-    HIDDEN_NODES_NUM = 3
-    OUTPUT_NODES_NUM = 3
+    INPUT_NODES_NUM = 784
+    HIDDEN_NODES_NUM = 200
+    OUTPUT_NODES_NUM = 10
     # initialise a learning rate, better be a smaller value (not zero)
-    LEARNING_RATE = 0.3
+    LEARNING_RATE = 0.1
 
     # create instance of neural network
     neural_network = MyEasyNetwork(INPUT_NODES_NUM, HIDDEN_NODES_NUM, OUTPUT_NODES_NUM, LEARNING_RATE)
 
-    # test inference (doesn't mean anything useful yet)
-    print(neural_network.inference([1.0, 0.5, -1.5]))
+    # load the mnist training data CSV file into a list
+    #training_data_file = open("data_set/mnist_train_100.csv", 'r')
+    training_data_file = open("data_set/mnist_train.csv", 'r')
+    training_data_list = training_data_file.readlines()
+    training_data_file.close()
+
+    # train the neural network
+    # epochs is the number of times the training data set is used for training
+    epochs = 5
+
+    for e in range(epochs):
+        # go through all records in the training data set
+        for record in training_data_list:
+            # split the record by the ',' commas
+            all_values = record.split(',')
+            # scale and shift the inputs
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            # create the target output values (all 0.01, except the desired label which is 0.99)
+            targets = numpy.zeros(OUTPUT_NODES_NUM) + 0.01
+            # all_values[0] is the target label for this record
+            targets[int(all_values[0])] = 0.99
+            neural_network.train(inputs, targets)
+            pass
+        pass
+
+
+     # load the mnist test data CSV file into a list
+    #test_data_file = open("data_set/mnist_test_10.csv", 'r')
+    test_data_file = open("data_set/mnist_test.csv", 'r')
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+
+    # test the neural network
+    # scorecard for how well the network performs, initially empty
+    scorecard = []
+
+    # go through all the records in the test data set
+    for record in test_data_list:
+        # split the record by the ',' commas
+        all_values = record.split(',')
+        # correct answer is first value
+        correct_label = int(all_values[0])
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # query the network
+        outputs = neural_network.inference(inputs)
+        # the index of the highest value corresponds to the label
+        label = numpy.argmax(outputs)
+        # append correct or incorrect to list
+        if (label == correct_label):
+            # network's answer matches correct answer, add 1 to scorecard
+            scorecard.append(1)
+        else:
+            # network's answer doesn't match correct answer, add 0 to scorecard
+            scorecard.append(0)
+            pass
+
+        pass
+
+    # calculate the performance score, the fraction of correct answers
+    scorecard_array = numpy.asarray(scorecard)
+    print ("performance = ", scorecard_array.sum() / scorecard_array.size)
